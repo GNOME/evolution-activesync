02eab2645ee2d16846d58f54829ca4b49477eaef
Lorna McNeill <lorna@lorna-0100.(none)>
2011-05-03 12:05:13 +0100

    fix mistake on FolderSync function (was no way to id the folder to be syncd)

--- libeasmail/libeasmail.h
+++ libeasmail/libeasmail.h
@@ -9,10 +9,14 @@ A draft API for the EAS mail client library which clients will use to get email 
 
 
 struct EasFolder {
-	gchar *parent__id;
+	gchar *parent_id;
 	gchar *server_id;		// from AS server. string up to 64 characters
 	gchar *display_name;
 };
+
+#define EAS_EMAIL_READ		0x00000001		// whether email has been read
+#define EAS_EMAIL_ANSWERED	0x00000002		// not clear how AS supports answered/forwarded!
+#define EAS_EMAIL_FORWARDED	0x00000004
 
 struct EasEmail {
 {
@@ -20,7 +24,7 @@ struct EasEmail {
 	gchar *mime_headers;		// for created email will be a string of mime headers separated by crlfs. Immutable
 	GSLIst *attachment_ids;		// list of attachments this email has. AS calls id the 'file reference' 
 	guint8	flags;			// bitmap. read, answered, forwarded
-	GSLIst *categories;		// array of category strings
+	GSLIst *categories;		// list of categories (strings) that the email belongs to 
 };
 
 // pulls down changes in folder structure (folders added/deleted/updated)
@@ -31,18 +35,17 @@ gboolean SyncFolderHierarchy(gchar *sync_key,
 			GError **error);
 
 
-/* sync emails in a folder (no bodies retrieved). Returns lists of EasEmails. 
+/* sync emails in a specified folder (no bodies retrieved). Returns lists of EasEmails. 
 In the case of created emails all fields are filled in.
 In the case of deleted emails only the serverids are filled in. 
 In the case of updated emails only the serverids, flags and categories are filled in.
 */
-gboolean FolderSync(guint max_entries,
-	gboolean *more_available,
+gboolean FolderSync(gchar *folder_id,	// folder to sync
 	GSList **emails_created,
-	GSList **emails_updated,		// updated flags or categories
+	GSList **emails_updated,	// updated flags or categories
 	GSList **emails_deleted,
-	guint	max_changes,			// max changes we want from server 0..512. AS calls window_size. 
-	guint	time_filter,			// AS calls filter_type. Eg "sync back 3 days". Required now?
+	// guint	max_changes,		// max changes we want from server 0..512. AS calls window_size. Required on api? AS defaults to 100
+	guint	time_filter,		// AS calls filter_type. Eg "sync back 3 days". Required on API?
 	gboolean *more_available,
 	GError **error);
 
@@ -83,18 +86,6 @@ Note that the only valid changes are to flags / categories (other changes ignore
 gboolean UpdateEmails(GSList *update_emails,
 				GError **error);
 
-// supported on AS?
-/*
-gboolean UpdateEmailJunk(const GSList *email_ids, gboolean junk,
-		GError **error);
-*/
-
-// supported on AS?
-/*
-gboolean UpdateEmailFollowup(const GSList *email_ids, gboolean followup,
-		GError **error);
-*/
-
 SendEmail(const gchar *email_id,	// supplied by client in this case
 	const gchar *mime_file,		// the full path to the email (mime) to be sent
 	const gchar *email_account,
@@ -113,10 +104,8 @@ CopyToFolder((const GSList *email_ids,
 	GError **error);
 
 /*
-
 Outstanding issues:
 How do drafts work (AS docs say email can't be added to server using sync command with add)?
-How do we implement 'junk' / 'followup' in AS (do we need to)?
 How does AS expose 'answered' / 'forwarded'? investigate email2:ConversationIndex (timestamps are added when email replied to/forwarded but not clear how to distinguish between those two things?
 */
 
