3d5d9e16e7f947fe15c7d33c8513bfb65803bce9
Terence McArt <tez@mobica53.mobicauk.local>
2011-05-19 15:45:42 +0100

    Adding in logic for Provision request

--- eas-daemon/libeas/eas-provision-req.c
+++ eas-daemon/libeas/eas-provision-req.c
@@ -8,9 +8,16 @@
 #include "eas-provision-req.h"
 #include "eas-provision-msg.h"
 
+typedef enum {
+	EasProvisionStep1 = 0,
+	EasProvisionStep2
+} EasProvisionState;
+
 struct _EasProvisionReqPrivate
 {
 	EasProvisionMsg* msg;
+	EasProvisionState state;
+	
 };
 
 #define EAS_PROVISION_REQ_PRIVATE(o)  (G_TYPE_INSTANCE_GET_PRIVATE ((o), EAS_TYPE_PROVISION_REQ, EasProvisionReqPrivate))
@@ -28,6 +35,7 @@ eas_provision_req_init (EasProvisionReq *object)
 	object->priv = priv = EAS_PROVISION_REQ_PRIVATE(object);
 
 	priv->msg = NULL;
+	priv->state = EasProvisionStep1;
 }
 
 static void
@@ -59,19 +67,83 @@ eas_provision_req_class_init (EasProvisionReqClass *klass)
 EasProvisionReq*
 eas_provision_req_new (gchar* policy_status, gchar* policy_key)
 {
-	/* TODO: Add public function implementation here */
+	EasProvisionReqPrivate *priv = NULL;
+	EasProvisionReq* req = NULL;
+
+	req = g_object_new(EAS_TYPE_PROVISION_REQ, NULL);
+	priv = req->priv;
+
+	// Build the message
+	priv->msg = eas_provision_msg_new ();
+	eas_provision_msg_set_policy_status (priv->msg, policy_status);
+	eas_provision_msg_set_policy_key (priv->msg, policy_key);
+
+	return req;
 }
 
 void
 eas_provision_req_Activate (EasProvisionReq* self)
 {
-	/* TODO: Add public function implementation here */
+	EasProvisionReqPrivate *priv = self->priv;
+	xmlDoc *doc = NULL;
+
+	doc = eas_provision_msg_build_message (priv->msg);
+
+	// TODO
+//	eas_connection_send_request(priv->cnc, doc, self);
+	xmlFree(doc);
 }
 
+/**
+ * @param doc The protocol xml to be parsed. MUST be freed with xmlFree()
+ */
 void
 eas_provision_req_MessageComplete (EasProvisionReq* self, xmlDoc *doc)
 {
-	/* TODO: Add public function implementation here */
+	EasProvisionReqPrivate *priv = self->priv;
+
+	eas_provision_msg_parse_response (priv->msg, doc);
+
+	switch (priv->state) {
+		default:
+		{
+			g_assert(0);
+		}
+		break;
+
+		// We are receiving the temporary policy key and need to now make a 
+		// second provision msg and send it using the new data.
+		case EasProvisionStep1:
+		{
+			EasProvisionMsg *msg = NULL;
+			
+			msg = eas_provision_msg_new ();
+			eas_provision_msg_set_policy_status (msg, eas_provision_msg_get_policy_status (priv->msg));
+			eas_provision_msg_set_policy_key (msg, eas_provision_msg_get_policy_key (priv->msg));
+
+//			eas_connection_set_policy_key(cnc, eas_provision_msg_get_policy_key (priv->msg));
+
+			g_object_unref(priv->msg);
+			priv->msg = msg;
+
+			priv->state = EasProvisionStep2;
+
+			eas_provision_req_Activate (self);
+		}
+		break;
+
+		// We no have the final provisioning policy key
+		// Set the policy key in the connection, allowing the original request
+		// from the daemon that triggered the provisioning to proceed.
+		case EasProvisionStep2:
+		{
+		//	eas_connection_set_policy_key(cnc, eas_provision_msg_get_policy_key (priv->msg));
+		// eas_continue_original_request(cnc);
+		}
+		break;
+	}
+
+	xmlFree(doc);
 }
 
 gchar*
