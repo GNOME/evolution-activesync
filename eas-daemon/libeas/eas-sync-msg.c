85016b6d219464a248d1dd01e3cef2626a2a3efa
David Woodhouse <David.Woodhouse@intel.com>
2011-06-17 14:39:37 +0100

    Parse <MoreAvailable/> node in Sync. Need to plumb this up so client sees it...

--- eas-daemon/libeas/eas-sync-msg.c
+++ eas-daemon/libeas/eas-sync-msg.c
@@ -16,6 +16,7 @@ struct _EasSyncMsgPrivate
 	GSList* updated_items;
 	GSList* deleted_items;
 
+	gboolean more_available;
 	gchar* sync_key;
 	gchar* folderID;
 	gint   account_id;
@@ -38,6 +39,7 @@ eas_sync_msg_init (EasSyncMsg *object)
 
 	object->priv = priv = EAS_SYNC_MSG_PRIVATE(object);
 	
+	priv->more_available = FALSE;
 	priv->sync_key = NULL;
 	priv->folderID = NULL;
 	priv->account_id = -1;
@@ -78,6 +80,7 @@ eas_sync_msg_new (const gchar* syncKey, const gint accountId, const gchar *folde
 	msg = g_object_new (EAS_TYPE_SYNC_MSG, NULL);
 	priv = msg->priv;
 
+	priv->more_available = FALSE;
 	priv->sync_key = g_strdup(syncKey);
 	priv->account_id = accountId;
 	priv->folderID = g_strdup(folderID);
@@ -370,6 +373,12 @@ eas_sync_msg_parse_response (EasSyncMsg* self, xmlDoc *doc, GError** error)
 			g_debug ("Got SyncKey = %s", priv->sync_key);
 			continue;
 		}
+        if (node->type == XML_ELEMENT_NODE && !g_strcmp0((char *)node->name, "MoreAvailable"))
+		{
+			priv->more_available = TRUE;
+			g_debug ("Got <MoreAvailable/>");
+			continue;
+		}
 		if (node->type == XML_ELEMENT_NODE && !g_strcmp0((char *)node->name, "Responses")) 
         {
                g_debug ("Responses:\n");
@@ -603,4 +612,12 @@ eas_sync_msg_get_syncKey(EasSyncMsg* self)
 	return priv->sync_key;
 }
 
+gboolean
+eas_sync_msg_get_more_available (EasSyncMsg *self)
+{
+	EasSyncMsgPrivate *priv = self->priv;
+	g_debug ("eas_sync_msg_get_more_available +-");
+	return priv->more_available;
+
+}
 
