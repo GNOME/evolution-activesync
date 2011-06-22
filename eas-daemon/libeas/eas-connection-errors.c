b3afd79ccead727d416a706c09c83816522d55fe
Tez McArt <tez.mcart@mobica.com>
2011-06-22 15:09:12 +0100

    Code review changes for eas-delete-email-req

--- eas-daemon/libeas/eas-connection-errors.c
+++ eas-daemon/libeas/eas-connection-errors.c
@@ -1,21 +1,4 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
-/*
- * eas-daemon
- * Copyright (C)  2011 <>
- *
- * eas-daemon is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * eas-daemon is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 
 #include "eas-connection-errors.h"
 
@@ -31,8 +14,6 @@ GQuark eas_connection_error_quark (void)
 
     return quark;
 }
-
-// TODO think of a more elegant way to do the status->error mappings?
 
 EasError common_status_error_map[] =
 {
--- eas-daemon/libeas/eas-delete-email-req.h
+++ eas-daemon/libeas/eas-delete-email-req.h
@@ -1,9 +1,4 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
-/*
- * code
- * Copyright (C)  2011 <>
- * 
- */
 
 #ifndef _EAS_DELETE_EMAIL_REQ_H_
 #define _EAS_DELETE_EMAIL_REQ_H_
@@ -37,10 +32,68 @@ struct _EasDeleteEmailReq
 };
 
 GType eas_delete_email_req_get_type (void) G_GNUC_CONST;
-EasDeleteEmailReq *eas_delete_email_req_new (const gchar* accountId, const gchar *syncKey, const gchar *folderId, const GSList *server_ids_array, EFlag *flag);
-gboolean eas_delete_email_req_Activate (EasDeleteEmailReq *self, GError** error);
-gboolean eas_delete_email_req_ActivateFinish (EasDeleteEmailReq* self, gchar** ret_sync_key, GError** error);
-void eas_delete_email_req_MessageComplete (EasDeleteEmailReq *self, xmlDoc* doc, GError* error);
+
+/** 
+ * Create a new delete email request GObject
+ *
+ * @param[in] account_id
+ *	  Unique identifier for a user account.
+ * @param[in] sync_key
+ *	  The current synchronisation key.
+ * @param[in] folder_id
+ *	  The identifer for the target server folder.
+ * @param[in] server_ids_array
+ *	  A list of email item server ids to be deleted.
+ * @param[in] flag
+ *	  A semaphore used to make the request appear synchronous by waiting for the
+ *	  server response. It should be set by the caller immediately after this 
+ *	  function is called and cleared in this request's MessageComplete.
+ *
+ * @return An allocated EasAddCalendarReq GObject or NULL
+ */
+EasDeleteEmailReq *eas_delete_email_req_new (const gchar* accountId, 
+                                             const gchar *syncKey, 
+                                             const gchar *folderId, 
+                                             const GSList *server_ids_array, 
+                                             EFlag *flag);
+
+/**
+ * Builds the messages required for the request and sends the request to the server.
+ *
+ * @param[in] self
+ *	  The EasDeleteEmailReq GObject instance to be Activated.
+ * @param[out] error
+ *	  GError may be NULL if the caller wishes to ignore error details, otherwise
+ *	  will be populated with error details if the function returns FALSE. Caller
+ *	  should free the memory with g_error_free() if it has been set. [full transfer]
+ *
+ * @return TRUE if successful, otherwise FALSE.
+ */
+gboolean eas_delete_email_req_Activate (EasDeleteEmailReq *self, 
+                                        GError** error);
+
+gboolean eas_delete_email_req_ActivateFinish (EasDeleteEmailReq* self, 
+                                              gchar** ret_sync_key, 
+                                              GError** error);
+
+/**
+ * Called from the Soup thread when we have the final response from the server.
+ *
+ * Responsible for parsing the server response with the help of the message and
+ * then clearing the semaphore to allow the caller that activated the request
+ * to continue.
+ *
+ * @param[in] self
+ *	  The EasDeleteEmailReq GObject instance whose messages are complete.
+ * @param[in] doc
+ *	  Document tree containing the server's response. This must be freed using
+ *	  xmlFreeDoc(). [full transfer]
+ * @param[in] error
+ *	  A GError code that has been propagated from the server response.
+ */
+void eas_delete_email_req_MessageComplete (EasDeleteEmailReq *self, 
+                                           xmlDoc* doc, 
+                                           GError* error);
 
 G_END_DECLS
 
