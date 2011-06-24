3976c20dc08515564553d6954132af6f7686b80a
David Woodhouse <David.Woodhouse@intel.com>
2011-06-25 00:03:03 +0100

    do not ask for password in configtool

--- configtool/activesync-setup.sh
+++ configtool/activesync-setup.sh
@@ -5,10 +5,10 @@ read -r -p "Enter email address: " EMAILADDR
 
 read -r -p "Enter username: " ASUSERNAME
 
-# Shouldn't ask for this in advance; SSO might work, and we 
-# should prompt when we need it
-read -r -p "Enter password: " -s ASPASSWD
-echo
+# Shouldn't ask for this in advance; SSO might work, and the dæmon will
+# if it needs it.
+#read -r -p "Enter password: " -s ASPASSWD
+#echo
 
 # We should do autodiscover here to find the URL
 read -r -p "Enter server URL: " SERVERURL
@@ -18,7 +18,7 @@ read -r -p "Enter server URL: " SERVERURL
 # Would be nice if it were like this...
 gconftool-2 --set --type=string /apps/activesyncd/accounts/$EMAILADDR/username "$ASUSERNAME"
 gconftool-2 --set --type=string /apps/activesyncd/accounts/$EMAILADDR/serverUri "$SERVERURL"
-gconftool-2 --set --type=string /apps/activesyncd/accounts/$EMAILADDR/password "$ASPASSWD"
+#gconftool-2 --set --type=string /apps/activesyncd/accounts/$EMAILADDR/password "$ASPASSWD"
 
 # Add Evolution account in /apps/evolution/mail/accounts with URL
 # eas:///account_uid=$EMAILADDR;check_all
