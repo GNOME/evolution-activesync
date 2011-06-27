474369dc652e84b228e03204ab51625986f72636
Salvatore Iovene <salvatore.iovene@linux.intel.com>
2011-06-26 23:48:05 -0700

    Autotools: added boost detection (only enabled if syncevolution is).
    Unfortunately boost lacks any form of pkg-config integration; ideally,
    syncevolution would drag boost-base as its dependency, but I couldn't
    quickly find a way for this to happen, so we're doing it here for the
    moment.

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
-autoreconf --force --install -I config
+
+set -e
+
+# wipe out temporary autotools files, necessary
+# when switching between distros
+rm -rf aclocal.m4 m4 autom4te.cache config.guess config.sub config.h.in configure depcomp install-sh ltmain.sh missing
+
+# intltoolize fails to copy its macros unless m4 exits
+mkdir m4
+
+libtoolize -c
+glib-gettextize --force --copy
+intltoolize --force --copy --automake
+aclocal -I m4 -I m4-repo
+autoheader
+automake -a -c -Wno-portability
+autoconf
 
