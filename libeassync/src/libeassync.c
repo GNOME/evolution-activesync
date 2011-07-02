2ee8deb0821d1c19b3639fcf8a427bf148a70968
David Woodhouse <David.Woodhouse@intel.com>
2011-07-02 09:55:50 +0100

    Add copyright licensing boilerplate to all files in libeassync

--- libeassync/src/libeassync.c
+++ libeassync/src/libeassync.c
@@ -1,8 +1,25 @@
-/**
+/*
+ * ActiveSync client library for calendar/addressbook synchronisation
  *
- *  Filename: libeassync.c
- *  Project:
- *  Description: client side library for eas calendar (wraps dbus api)
+ * Copyright © 2011 Intel Corporation.
+ *
+ * Authors: Mobica Ltd. <www.mobica.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later
+ * version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
  *
  */
 
