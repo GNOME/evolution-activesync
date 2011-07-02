77e0ff3de13d5de313cdc7dc1f26960bc0f34693
David Woodhouse <David.Woodhouse@intel.com>
2011-07-02 09:52:49 +0100

    Add copyright licensing boilerplate to all files in libeasaccount

--- libeasaccount/src/eas-account-list.h
+++ libeasaccount/src/eas-account-list.h
@@ -1,4 +1,26 @@
-
+/*
+ * ActiveSync account management
+ *
+ * Copyright © 2011 Intel Corporation.
+ *
+ * Derived from code in Evolution's libedataserver library, marked:
+ * Copyright © 1999-2008 Novell, Inc. (www.novell.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of version 2.1 of the GNU Lesser General
+ * Public License as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
+ *
+ */
 
 #ifndef __EAS_ACCOUNT_LIST__
 #define __EAS_ACCOUNT_LIST__
