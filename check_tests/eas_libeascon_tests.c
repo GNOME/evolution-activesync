0874532f06976beb8386a3a927c842197da07ff0
Brahim Abes <brahim.abes@mobica.com>
2011-07-14 16:45:14 +0100

    add evolution vcard test

--- check_tests/eas_libeascon_tests.c
+++ check_tests/eas_libeascon_tests.c
@@ -10,15 +10,141 @@
 #include "../libeassync/src/eas-item-info.h"
 #include "../eas-daemon/libeas/eas-con-info-translator.h"
 
-gchar * g_contacts_account_id = TEST_ACCOUNT_ID;
+gchar * g_account_id = TEST_ACCOUNT_ID;
 
+const char* TEST_VCARD_FROM_EVO = "BEGIN:VCARD\n\
+VERSION:3.0\n\
+LABEL;TYPE=OTHER:myOtherAddress \nmyOtherCity\, myOtherState\nmyOtherZip\nm\
+yOtherPOBox\nmyOtherCountry\n\
+ADR;TYPE=OTHER:myOtherPOBox;;myOtherAddress ;myOtherCity;myOtherState;myOth\
+erZip;myOtherCountry\n\
+LABEL;TYPE=HOME:myHomeAddress \nmyHomeCity\, myHomeState\nmyHomeZip\nmyHome\
+POBox\nmyHomeCountry\n\
+ADR;TYPE=HOME:myHomePOBox;;myHomeAddress ;myHomeCity;myHomeState;myHomeZip;\
+myHomeCountry\n\
+LABEL;TYPE=WORK:myWorkAddress\nmyWorkCity\, myWorkState\nmyWorkZip\nmyWorkP\
+OBox\nmyWorkCountry\n\
+ADR;TYPE=WORK:myWorkPOBox;;myWorkAddress;myWorkCity;myWorkState;myWorkZip;m\
+yWorkCountry\n\
+X-ICQ;X-EVOLUTION-UI-SLOT=4;TYPE=HOME:myICQ\n\
+X-MSN;X-EVOLUTION-UI-SLOT=3;TYPE=HOME:myMSN\n\
+X-YAHOO;X-EVOLUTION-UI-SLOT=2;TYPE=HOME:myYahoo\n\
+X-AIM;X-EVOLUTION-UI-SLOT=1;TYPE=HOME:myAIM\n\
+TEL;X-EVOLUTION-UI-SLOT=4;TYPE=HOME,VOICE:20123456789\n\
+TEL;X-EVOLUTION-UI-SLOT=3;TYPE=WORK,FAX:40123456789\n\
+TEL;X-EVOLUTION-UI-SLOT=2;TYPE=CELL:30123456789\n\
+TEL;X-EVOLUTION-UI-SLOT=1;TYPE=WORK,VOICE:10123456789\n\
+EMAIL;X-EVOLUTION-UI-SLOT=4;TYPE=OTHER:myemail@Other.com\n\
+EMAIL;X-EVOLUTION-UI-SLOT=3;TYPE=HOME:myemail2@Home.com\n\
+EMAIL;X-EVOLUTION-UI-SLOT=2;TYPE=HOME:myemail@Home.com\n\
+EMAIL;X-EVOLUTION-UI-SLOT=1;TYPE=WORK:myemail@Work.com\n\
+PHOTO;ENCODING=b;TYPE=\"X-EVOLUTION-UNKNOWN\":/9j/4AAQSkZJRgABAQEASABIAAD/4QM\
+IRXhpZgAASUkqAAgAAAAKAA4BAgAgAAAAhgAAAA8BAgAFAAAApgAAABABAgAOAAAArAAAABIBA\
+wABAAAAAQAAABoBBQABAAAAugAAABsBBQABAAAAwgAAACgBAwABAAAAAgAAADIBAgAUAAAAygA\
+AABMCAwABAAAAAgAAAGmHBAABAAAA3gAAAGICAAAgICAgICAgICAgICAgICAgICAgICAgICAgI\
+CAgICAgAFNPTlkAAENZQkVSU0hPVAAAAAAASAAAAAEAAABIAAAAAQAAADIwMDM6MDY6MDQgMTc\
+6MDA6MDIAFgCaggUAAQAAAOwBAACdggUAAQAAAPQBAAAiiAMAAQAAAAIAAAAniAMAAQAAAFAAA\
+AAAkAcABAAAADAyMTADkAIAFAAAAPwBAAAEkAIAFAAAABACAAABkQcABAAAAAECAwACkQUAAQA\
+AACQCAAAEkgoAAQAAACwCAAAFkgUAAQAAADQCAAAHkgMAAQAAAAIAAAAIkgMAAQAAAAAAAAAJk\
+gMAAQAAAAAAAAAKkgUAAQAAADwCAAAAoAcABAAAADAxMDABoAMAAQAAAAEAAAACoAQAAQAAAEA\
+GAAADoAQAAQAAALAEAAAAowcAAQAAAAMAAAABowcAAQAAAAEAAAAFoAQAAQAAAEQCAAAAAAAAA\
+QAAADIAAAAcAAAACgAAADIwMDM6MDY6MDQgMTc6MDA6MDIAMjAwMzowNjowNCAxNzowMDowMgA\
+CAAAAAQAAAAAAAAAKAAAAAwAAAAEAAAA9AAAACgAAAAIAAQACAAQAAABSOTgAAgAHAAQAAAAwM\
+TAwAAAAAAgAAwEDAAEAAAAGAAAADwECAAUAAADIAgAAEAECAA4AAADOAgAAEgEDAAEAAAABAAA\
+AGgEFAAEAAADcAgAAGwEFAAEAAADkAgAAKAEDAAEAAAACAAAAMgECABQAAADsAgAAAAAAAFNPT\
+lkAAENZQkVSU0hPVAAAAAAASAAAAAEAAABIAAAAAQAAADIwMDM6MDY6MDQgMTc6MDA6MDIA/9s\
+AQwAFAwQEBAMFBAQEBQUFBgcMCAcHBwcPCwsJDBEPEhIRDxERExYcFxMUGhURERghGBodHR8fH\
+xMXIiQiHiQcHh8e/9sAQwEFBQUHBgcOCAgOHhQRFB4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4\
+eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4e/8AAEQgAYABgAwEiAAIRAQMRAf/EAB0AAAICAwEBA\
+QAAAAAAAAAAAAYHBAUCAwgJAAH/xAA+EAABAwMCBAUBBAYJBQAAAAABAgMEAAURBiESMUFhBxN\
+RcYEiFDKRsSM0UmKh0QgWFyUmM0KSwVNygrLw/8QAGgEAAwEBAQEAAAAAAAAAAAAAAwQFAgABB\
+v/EACYRAAICAQQCAgEFAAAAAAAAAAECABEDBBIxQQUhEzIiI1FhgZH/2gAMAwEAAhEDEQA/ALr\
+zktZJO/fpW5ElpaT9eTVA5IKj97av1t5KTj+NVrkUCXinknCFEEE8q2F4kYSeHvVGZWVBOeVE9\
+i05cpsRMqUsQIahlLjo+pY9Up5n3OB3rDOFFsYREZzSi5FTIUgYVv7VJRKDhGEEdyKsnItlgpw\
+lh2asc1POFIPsE4x+JqFIvDbOzUCEgDkC0FfxOTSbeRxKfQJja+Oyt2BM2n1ZAztU5mQgKBJ26\
+/SaozqVCT+ltcJwdklH/qRUqJe7HLIQ4JEBZ658xGfbYgfJrSeSwk0bEHk8XnAsUYSNSQAMJWe\
++MfnWaZSlAFRFVjiH47KZCFNyIyjhL7R4knseoPY4Na0ys7888qoI4YWJKyIyGmFGXaJSFIKQo\
+ZPrUV1ZIVxADfbeqxUoBQyDnp2rFczB4eLNbBgqiqTJOedbEyDyzVOHqKtB6cf1DKU86VtW9k4\
+ddA3Uf2E9+/QfAKrEKLMqKm40Id+EulWZsZWpLq0HIza+GKyofS4sc1H1AO2PUH03sPEHU9vs8\
+ZybdpzbDYzgrPPsB1oznO23T2gozw4Y8GJFU6Rk4AA3rz68W9eT9Y6klS3XVpjBZDDOdkJzt81\
+I1DtkahLOmxKgN8CMvV/juwl1bNkglwcvNeOM/FAk/wAY9UPLBbWy1tuAgGlkpbh6YrFPGe9DG\
+mXs3DnU19RGTH8XtSoXl4x3k+imwPyos034uW6W6hq6xVRFHbzEHKfw50i8K7Vk2N9z+Fc2lQ8\
+Tl1R7E7c0LqLhCZEGS3JiujC054kOJ9CKKdRw0RorN2t/F9gkHhKc5LK+fCT6c8HsR03498HtX\
+TLDqJiKpa3IchXC4gq2H73xXb+iozN405Pty3QpmRH40KA4uEjdKh67gVrSZHwZNp4MBr8CZ8e\
+4c9QFMniHOsfO71ovMWTapyokpIBG6FJ+6tPRQPpUIyAeu9fQA37E+XKkGjF7aWX7lco8CMOJ6\
+Q6ltA6ZJxv2rqDStjj2u0x4UZGGI6OEEjdauqj3J3pD+Ctrkf2ieTOjOsPwmHFlp1BSpKtkYIP\
+X6jXTTDZajraWjg8sDBJ+8D19jU/K1mpaxrtW4Fa/jSb54cag0zEfUzPYZcUwQkkltQyNgCSBt\
+nAJwFYB5V55agtU+zXNyDcWfKeSAoYUFJUk7hSVDZQI5EV6Kayblsus3e1u+XIj4HEDgEZ5HtS\
+Y8ZNEWTxbiMXKzOxbJq+IjylsyD5bEtAyeDIH0KBJwcY3wcDGEtyqdrf1HgGdbX3+85D6b1ig7\
+fNEms9Dau0fI8nUmn51vBOEvLb4mXP+xxOUK+CaG2+vvRIOZV8K+xU+x2a7XuciBZrZMuMpf3W\
+YrCnVn4SCa8nSK0SFg16L+Cwcs/hvFud4QGnPsyGSg53KRwnnvz2+DXPXg5/R3lwrhE1F4kutQ\
+IsdaXW7QhYW++RuA4RkITyyMlR3BCa6oagw9QWxt2YWWISP1eP5oQAkbAkDfFCai1jmG3UtHiV\
+F4XpDUcduLMjqjqSslt1h0hSM8/vZGOW3alhrayu6bvJhGQmSwtAcYfSMcaD26EciKYuq4umrf\
+Bcaajx/Oxs419JSexG5NKbVlzlvrixJhc8yO3n6xg4Xgj+GD80zo82T5PjPER12DGcfyD0ZeaW\
+8f03Oeg6n0bGkzEI/Xba4EO4BH+hfTrjjxtyppxtVafu1ubdj3Z+C2ocaGri0WkJyM8Ic3R6bc\
+VcR2O7i13eNNV9aWnQVpH+pHJQ/AmuoLAttiAzcLQ6ZNulICwkHOMjpWcv6bceoxjrIn8wuuMx\
+MY+UtbSw4k4AcCkrT2IO4pQ60heRczIjnCXDlKknG/pn1oqvtqtc0KK4aWXc7qby2sHvjG/vQH\
+f4V5gNr+xTzPY6x5Zyr4WOtKZ6cVGdPaNYku06x1DbkqYRPccYUMKad+pKh6EdfmtzknS9yVx3\
+LQGlpLivvOC2tIUr3KUgmgT+sMQpzJ44zgOFJcG6T6d6mwL3bnCOG4Mf7wKSG9eCZQJxvyBcN4\
+Vv0O2rjY8OtLpX0LkBDgHwrIopg36axE+x21mJbY3/RiMJaR/tSAKArdcYq8cMpCvY5q3N1SlA\
+Swkk/tK/lWwznkwDBBwIUtOF9RMmSoHG61b49qhXq7NxGeGPc5LqwMBISEj86HQ/LfUEpKjmra\
+1WPzFB2Tk9jWww4AswJVibJ9SBYbbqLUV1QtyUW2OLYEc/c8/wqr8VXv8ayWuIEtMR21HuGUZ/\
+jTPgTIdojSJj+G2I7KlKV0SnG/wA42Hc0gr3dXbteJlye2ckvLdI9MnOPjlVLxyUWaTvJPaqsT\
+0h84O9MrwR8URp98WC9O/3a6r9C4o/5Kj0Pb8qVLyj0qtlkgGiZAGFGbxfgbndk0MXNpMuI+PM\
+XuF8WUKH/AN+dCd6S4UKQ80UrHUcjXOHh14s33SKkRHlKn20HHkrV9SB+6f8Ain3pfxH0dqxpC\
+WZzbEkjdl76Vg+x51PyAjmOoA31/wAi48Q7dJLZlwm/McGziBzI9QPUUtESHA8RxFJBwUnYj3F\
+dVzdNwbigqbdbUCNiCKE7v4UNTHCvjbJ6FTYJFYUmqhLANxWaZvMyCUqYfKR+wTlJ+KcOippvs\
+QulgtrQeFWB9JPY/wDFQLP4RsRn0uOvoUAc8PkJIP45pk2a1RbVGDSSBjqqvBiXszsmYn6ibLR\
+aUjCiAAOZNXC20pR5bScbfUo0L6m1/pTS7Kjcrqz5wH0sNnjcV7JG/wA7CknrvxeumpwuBbUKt\
+tsXspAVl14fvEch2HzmmsWHf6XiKZcuz20MvFPWzFwJsFmf44ja8yX0HZ9Q5JHqkevU+1Arbwz\
+nND8F7CASanoeGaqY0VF2rJWRmyNuaLh13Y1DkK4hUhaTWhbZpEmUgJWujetYJScgkEciKmOt+\
+orQtqs3OqXVm1rqu0ACBfZjaU8kqXxp/BWaJ4fjXryOkJM2M93cZ/kRS7LavSvwIV6GslFPU2M\
+jjuMt3xv166nhEqI13Qz/ADJqkuniFrS75TN1DM4Vc0tKDQx/44oUQ2v9k1IaZPU16EUdTwu57\
+khtalucS1KWonJJOSauIWyk71WxmwnpVjG9qZQwLLL+K6EgDn61NQ6BvmqVlRwKlId2xTAMVZZ\
+//9k=\n\
+X-MOZILLA-HTML:FALSE\n\
+X-EVOLUTION-VIDEO-URL:myVideoChat\n\
+FBURL:myFreeOrBusy\n\
+CALURI:myCalendar\n\
+ROLE:myProfession\n\
+X-COUCHDB-APPLICATION-ANNOTATIONS:{ \"Evolution\" : { \"revision\" : \"2011-07-1\
+0T00:28:58Z\" } }\n\
+REV:2011-07-10T00:28:58Z\n\
+X-EVOLUTION-ANNIVERSARY:2011-07-13\n\
+BDAY:1995-07-13\n\
+X-EVOLUTION-BLOG-URL;X-COUCHDB-UUID=\"8930d503-0794-454b-9c4e-f30b093654fc\":\
+myWebLog\n\
+URL;X-COUCHDB-UUID=\"84fb728f-9e33-4dbb-af66-e62dedfbcc1c\":http://www.myHome\
+Page.com\n\
+NOTE:myNotes: this is a test contact for the Contacts translator\n\
+CATEGORIES:Competition,Favorites,Birthday,Anniversary,Business\n\
+X-EVOLUTION-ASSISTANT:myAssistant\n\
+X-EVOLUTION-MANAGER:myManager\n\
+TITLE:myTitle\n\
+ORG:myCompany;myDepartment;myOffice\n\
+X-EVOLUTION-SPOUSE:mySpouse\n\
+NICKNAME:myNickname\n\
+FN:Mr. myFirstName myMiddleName myLastName Sr.\n\
+X-EVOLUTION-FILE-AS:myLastName\, myFirstName\n\
+N:myLastName;myFirstName;myMiddleName;Mr.;Sr.\n\
+UID:pas-id-4E1E1CFD00000000\n\
+X-COUCHDB-REVISION:3-27f3c9f29571145454ce17f436b948c3\n\
+END:VCARD\n";
 
-const char* TEST_VCARD_01 = "BEGIN:vCard\n\
+#if 0
+const char* TEST_VCARD = "BEGIN:vCard\n\
 VERSION:3.0\n\
 FN:John Stevenson\n\
 N:Stevenson;John;Philip,Paul;Dr.;Jr.,M.D.,A.C.P.\n\
+NICKNAME:Jim,Jimmie\n\
+TITLE:Director\n\
+ROLE:Programmer\n\
 ORG:Lotus Development Corporation\n\
-ADR;TYPE=WORK,POSTAL,PARCEL:;;6544 Battleford Drive\
+ADR;TYPE=PARCEL,POSTAL,WORK:;;6544 Battleford Drive\
+ ;Raleigh;NC;27613-3502;U.S.A.\n\
+ADR;TYPE=POSTAL,HOME,PARCEL:;;6544 Battleford Drive\
+ ;Raleigh;NC;27613-3502;U.S.A.\n\
+ADR;TYPE=POSTAL,OTHER,PARCEL:;;6544 Battleford Drive\
  ;Raleigh;NC;27613-3502;U.S.A.\n\
 TEL;TYPE=VOICE,MSG,WORK:+1-919-676-9515\n\
 TEL;TYPE=FAX,WORK:+1-919-676-9564\n\
@@ -26,61 +152,51 @@ EMAIL;TYPE=INTERNET,PREF:Frank_Dawson@Lotus.com\n\
 EMAIL;TYPE=INTERNET:fdawson@earthlink.net\n\
 URL:http://home.earthlink.net/~fdawson\n\
 BDAY:1996-04-15\n\
-PHOTO;VALUE=URL;TYPE=GIF:http://www.example.com/dir_photos/my_photo.gif\n\
+NOTE:This fax number is operational 0800 to 1715\n\
 PHOTO;ENCODING=b;TYPE=JPEG:MIICajCCAdOgAwIBAgICBEUwDQYJKoZIhvcN\
 AQEEBQAwdzELMAkGA1UEBhMCVVMxLDAqBgNVBAoTI05ldHNjYXBlIENvbW11bm\
 ljYXRpb25zIENvcnBvcmF0aW9uMRwwGgYDVQQLExNJbmZvcm1hdGlvbiBTeXN0\n\
-TITLE:Shrimp Man\n\
-NOTE:This fax number is operational 0800 to 1715\n\
-END:vCard";
-
-
-const char* TEST_VCARD = "BEGIN:vCard\n\
-VERSION:3.0\n\
-N:Stevenson;John;Philip,Paul;Dr.;Jr.,M.D.,A.C.P.\n\
-ADR;TYPE=WORK,POSTAL,PARCEL:;;6544 Battleford Drive \
-;Raleigh;NC;27613-3502;U.S.A.\n\
-ADR;TYPE=HOME,POSTAL,PARCEL:;;6544 Battleford Drive \
-;Raleigh;NC;27613-3502;U.S.A.\n\
-ADR;TYPE=HOME,POSTAL,PARCEL:;;6544 Battleford Drive \
-;Raleigh;NC;27613-3502;U.S.A.\n\
 END:vCard\n";
+#endif
 
-
-/*vCard from ActiveSync Response*/
-const char* TEST_VCARD_FROM_AS ="BEGIN:VCARD\n\
-VERSION:3.0\r\n\
-ROLE:bricky\n\
-TEL;TYPE=WORK:6574165464165\n\
-TEL;TYPE=WORK:0161 111 2222\n\
-TEL;TYPE=PAGER:pager #\n\
-TEL;TYPE=CAR:645646346544\n\
-ADR;TYPE=OTHER:other city;other country;other post code;other state;other street\n\
-ORG:Company 1\n\
-TEL;TYPE=CELL:0777 333 4444\n\
-TEL;TYPE=HOME:567573\n\
-TEL;TYPE=HOME:0161 222 3333\n\
-TEL;TYPE=HOME,FAX:ahhhh\, he'll save everyone of us\n\
-ADR;TYPE=HOME:Wilmslow;UK;SK9 1AY;Cheshire;Market House Church Street\n\
-N:bob;the;builder\n\
-EMAIL:\"bob.the.builder@cstylianou.com\" <bob.the.builder@cstylianou.com>\n\
-ADR;TYPE=WORK:Business City;Business Country;Business Postal Code;Business \n\
- State;Business Address Street\n\
-URL:www.bobthebuilder.com\n\
+#if 0
+const char* TEST_VCARD = "BEGIN:VCARD\n\
+N:myFamilyName;myFirstName;myMiddleName;myTitle;mySuffix\n\
+ADR;INTL;PARCEL;WORK:;;myStreet;myCity;myStateOrProvince;myZipOrPostalCode;myCountryOrRegion\n\
 END:VCARD\n";
+#endif
 
-/*vCard from Evolution*/
-const char* TEST_VCARD_FROM_EVO = "BEGIN:VCARD\n\
-VERSION:3.0\n\
-REV:2011-05-23T15:08:32Z\n\
-UID:pas-id-4DDA786E00000000\n\
-N:Abes;Brahim;;;\n\
-FN:Brahim Abes\n\
-NICKNAME:brahim\n\
-X-EVOLUTION-FILE-AS:Abes\, Brahim\n\
-URL:\n\
-ADR;TYPE=HOME:;;;;;;\n\
-END:VCARD\n";
+/*
+ADR;DOM;PARCEL;HOME:;;myStreet;myCity;myStateOrProvince;myZipOrPostalCode;myCountryOrRegion\n\
+EMAIL;INTERNET:myEmail1@company.com\n\
+EMAIL;INTERNET:myEmail2@company.com\n\
+EMAIL;INTERNET:myEmail3@company.com\n\
+ORG:myCompany\n\
+TEL;WORK:myBusinessPhone\n\
+TEL;FAX;WORK:myBusinessFax\n\
+TEL;CELL:myMobilePhone\n\
+TEL;HOME:myHomePhone\n\
+TITLE:myJobTitle\n\
+URL;WORK:myWorkWebsite\n\
+URL:myPersonalWebsite\n\
+*/
+
+static gchar *
+random_uid_new (void)
+{
+	static gint serial;
+	static gchar *hostname;
+
+	if (!hostname) {
+		hostname = (gchar *) g_get_host_name ();
+	}
+
+	return g_strdup_printf ("%luA%luB%dC%s",
+				(gulong) time (NULL),
+				(gulong) getpid (),
+				serial++,
+				hostname);
+}
 
 static void testGetContactsHandler (EasSyncHandler **sync_handler, const gchar* accountuid)
 {
@@ -129,6 +245,7 @@ static void testGetLatestContacts (EasSyncHandler *sync_handler,
 
 START_TEST (test_eas_sync_handler_add_con)
 {
+#if 0
     EasSyncHandler *sync_handler = NULL;
     GError *error = NULL;
     gboolean testCalFound = FALSE;
@@ -159,7 +276,7 @@ START_TEST (test_eas_sync_handler_add_con)
 
     updatedconitem = eas_item_info_new();
     updatedconitem->client_id = g_strdup ("sdfasdfsdf");
-    updatedconitem->data = g_strdup (TEST_VCARD_01);//TEST_VCARD
+    updatedconitem->data = g_strdup (TEST_VCARD);//TEST_VCARD_01 
 
     conitemToUpdate = g_slist_append (conitemToUpdate, updatedconitem);
 
@@ -186,6 +303,70 @@ START_TEST (test_eas_sync_handler_add_con)
     testCalFound = TRUE;
 
     g_object_unref (sync_handler);
+#endif
+
+  	const char* accountuid = g_account_id;
+    EasSyncHandler *sync_handler = NULL;
+    GError *error = NULL;
+    gboolean testCalFound = FALSE;
+
+    // get a handle to the DBus interface and associate the account ID with
+    // this object
+    testGetContactsHandler (&sync_handler, accountuid);
+
+
+
+    gchar* folder_sync_key_in = NULL;
+	gchar* folder_sync_key_out = NULL;
+    GSList *calitems_created = NULL; //receives a list of EasMails
+    GSList *calitems_updated = NULL;
+    GSList *calitems_deleted = NULL;
+
+    testGetLatestContacts (sync_handler,
+                           folder_sync_key_in,
+                           &folder_sync_key_out,
+                           &calitems_created,
+                           &calitems_updated,
+                           &calitems_deleted,
+                           &error);
+
+    GSList *calitemToUpdate = NULL;
+    EasItemInfo *updatedcalitem = NULL;
+    gboolean rtn = FALSE;
+    gchar *random_uid = random_uid_new();
+
+    updatedcalitem = eas_item_info_new();
+    updatedcalitem->client_id = random_uid; // Pass ownership
+    updatedcalitem->data = g_strdup_printf(TEST_VCARD_FROM_EVO, random_uid);//TEST_VCARD
+
+    g_debug("Random UID:     [%s]", random_uid);
+    g_debug("TEST_VCARD_FROM_EVO	     [%s]", updatedcalitem->data);
+
+    calitemToUpdate = g_slist_append (calitemToUpdate, updatedcalitem);
+
+	g_free(folder_sync_key_in);
+	folder_sync_key_in = g_strdup(folder_sync_key_out);
+	g_free(folder_sync_key_out);
+	folder_sync_key_out = NULL;
+
+    rtn = eas_sync_handler_add_items (sync_handler, folder_sync_key_in, folder_sync_key_out, EAS_ITEM_CONTACT, NULL, calitemToUpdate, &error);
+    if (error)
+    {
+        fail_if (rtn == FALSE, "%s", error->message);
+    }
+    updatedcalitem = calitemToUpdate->data;
+    fail_if (updatedcalitem->server_id == NULL, "Not got new id for item");
+
+    g_slist_free (calitemToUpdate);
+
+	g_free(folder_sync_key_in);
+	g_free(folder_sync_key_out);
+
+
+    testCalFound = TRUE;
+
+    g_object_unref (sync_handler);
+	
 }
 END_TEST
 
@@ -193,6 +374,46 @@ END_TEST
 
 START_TEST (test_get_latest_contacts_items)
 {
+
+    // This value needs to make sense in the daemon.  in the first instance
+    // it should be hard coded to the value used by the daemon but later
+    // there should be a mechanism for getting the value from the same place
+    // that the daemon uses
+    const char* accountuid = g_account_id;
+    EasSyncHandler *sync_handler = NULL;
+
+    // get a handle to the DBus interface and associate the account ID with
+    // this object
+    testGetContactsHandler (&sync_handler, accountuid);
+
+    // declare lists to hold the folder information returned by active sync
+    GSList *created = NULL; //receives a list of EasFolders
+    GSList *updated = NULL;
+    GSList *deleted = NULL;
+    // Sync Key set to Zero.  This means that this is the first time the sync is being done,
+    // there is no persisted sync key from previous sync's, the returned information will be
+    // the complete folder hierarchy rather than a delta of any changes
+    gchar* sync_key_in = NULL;
+	gchar* sync_key_out = NULL;
+
+    GError *error = NULL;
+
+    mark_point();
+    // call into the daemon to get the folder hierarchy from the exchange server
+    testGetLatestContacts ( sync_handler, sync_key_in, &sync_key_out, &created, &updated, &deleted, &error);
+
+    //  free everything!
+    g_slist_foreach (created, (GFunc) g_object_unref, NULL);
+    g_slist_foreach (deleted, (GFunc) g_object_unref, NULL);
+    g_slist_foreach (updated, (GFunc) g_object_unref, NULL);
+
+    g_slist_free (created);
+    g_slist_free (deleted);
+    g_slist_free (updated);
+
+    g_object_unref (sync_handler);
+
+#if 0
     // This value needs to make sense in the daemon.  in the first instance
     // it should be hard coded to the value used by the daemon but later
     // there should be a mechanism for getting the value from the same place
@@ -229,6 +450,7 @@ START_TEST (test_get_latest_contacts_items)
     g_slist_free (updated);
 
     g_object_unref (sync_handler);
+#endif
 }
 END_TEST
 
@@ -236,7 +458,7 @@ END_TEST
 START_TEST (test_translate_vcard_to_xml)
 {
 	EasItemInfo contactInfo;// = eas_cal_info_new();
-    contactInfo.data = TEST_VCARD_01; //TEST_VCARD
+    contactInfo.data = TEST_VCARD_FROM_EVO;
     contactInfo.server_id = "1.0 (test value)";
 
     xmlDocPtr doc = xmlNewDoc ("1.0");
@@ -261,9 +483,9 @@ Suite* eas_libeascon_suite (void)
 
     //tcase_add_test (tc_libeascon, test_get_sync_handler);
 	//tcase_add_test (tc_libeascon, test_get_latest_contacts_items);
-   tcase_add_test (tc_libeascon, test_translate_vcard_to_xml);
+//	tcase_add_test (tc_libeascon, test_translate_vcard_to_xml);
     //tcase_add_test (tc_libeascon, test_eas_sync_handler_delete_con);
     //tcase_add_test (tc_libeascon, test_eas_sync_handler_update_con);
-    //tcase_add_test (tc_libeascon, test_eas_sync_handler_add_con);
+   tcase_add_test (tc_libeascon, test_eas_sync_handler_add_con);
     return s;
 }
