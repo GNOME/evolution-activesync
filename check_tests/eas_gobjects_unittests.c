b680148f7d2606c77f4e552cfbff6ac475f970b1
Jeremy Allen <jeremy.allen@mobica.com>
2011-07-15 11:23:59 +0100

    add unit tests for basic gobject sanity checks

--- check_tests/eas_gobjects_unittests.c
+++ check_tests/eas_gobjects_unittests.c
@@ -2,9 +2,45 @@
 #include <stdio.h>
 #include <string.h>
 #include <check.h>
+#include <glib-object.h>
 
 #include "../eas-daemon/libeas/eas-request-base.h"
+
 #include "../eas-daemon/libeas/eas-add-item-req.h"
+#include "../eas-daemon/libeas/eas-connection.h"
+#include "../eas-daemon/libeas/eas-delete-req.h"
+#include "../eas-daemon/libeas/eas-get-email-attachment-msg.h"
+#include "../eas-daemon/libeas/eas-get-email-attachment-req.h"
+#include "../eas-daemon/libeas/eas-get-email-body-msg.h"
+#include "../eas-daemon/libeas/eas-get-email-body-req.h"
+#include "../eas-daemon/libeas/eas-move-email-msg.h"
+#include "../eas-daemon/libeas/eas-move-email-req.h"
+#include "../eas-daemon/libeas/eas-msg-base.h"
+#include "../eas-daemon/libeas/eas-ping-msg.h"
+#include "../eas-daemon/libeas/eas-ping-req.h"
+#include "../eas-daemon/libeas/eas-provision-msg.h"
+#include "../eas-daemon/libeas/eas-provision-req.h"
+#include "../eas-daemon/libeas/eas-request-base.h"
+#include "../eas-daemon/libeas/eas-send-email-msg.h"
+#include "../eas-daemon/libeas/eas-send-email-req.h"
+#include "../eas-daemon/libeas/eas-sync-folder-hierarchy-req.h"
+#include "../eas-daemon/libeas/eas-sync-folder-msg.h"
+#include "../eas-daemon/libeas/eas-sync-msg.h"
+#include "../eas-daemon/libeas/eas-sync-req.h"
+#include "../eas-daemon/libeas/eas-update-email-req.h"
+#include "../eas-daemon/libeas/eas-update-item-req.h"
+#include "../eas-daemon/src/eas-mail.h"
+#include "../eas-daemon/src/eas-sync.h"
+#include "../eas-daemon/src/eas-test.h"
+#include "../libeasaccount/src/eas-account-list.h"
+#include "../libeasaccount/src/eas-account.h"
+#include "../libeasmail/src/eas-attachment.h"
+#include "../libeasmail/src/eas-email-info.h"
+#include "../libeasmail/src/eas-folder.h"
+#include "../libeasmail/src/libeasmail.h"
+#include "../libeassync/src/eas-item-info.h"
+#include "../libeassync/src/libeassync.h"
+#include "../libeastest/src/libeastest.h"
 
 
 START_TEST (test_add_item_req_obj)
@@ -14,10 +50,255 @@ START_TEST (test_add_item_req_obj)
     // oriented approach supported by G types
     g_type_init();
 
-	EasAddItemReq* self = g_object_new (EAS_TYPE_ADD_ITEM_REQ, NULL);
-	g_object_ref(self);
-	g_object_unref(self);
-	g_object_unref(self);	
+	g_debug("gobject: EAS_TYPE_ADD_ITEM_REQ ++");
+	EasAddItemReq* self_eas_add_item_req;
+	self_eas_add_item_req = g_object_new (EAS_TYPE_ADD_ITEM_REQ, NULL);
+	g_object_ref(self_eas_add_item_req);
+	g_object_unref(self_eas_add_item_req);
+	g_object_unref(self_eas_add_item_req);	
+	g_debug("EAS_TYPE_ADD_ITEM_REQ --");
+
+	g_debug("EAS_TYPE_CONNECTION ++");
+	EasConnection* self_eas_connection = g_object_new (EAS_TYPE_CONNECTION, NULL);
+	g_object_ref(self_eas_connection);
+	g_object_unref(self_eas_connection);
+	g_object_unref(self_eas_connection);	
+	g_debug("EAS_TYPE_CONNECTION --");
+
+	g_debug("EAS_TYPE_DELETE_REQ ++");
+	EasDeleteReq* self_eas_delete_req = g_object_new (EAS_TYPE_DELETE_REQ, NULL);
+	g_object_ref(self_eas_delete_req);
+	g_object_unref(self_eas_delete_req);
+	g_object_unref(self_eas_delete_req);	
+	g_debug("EAS_TYPE_DELETE_REQ --");
+
+	g_debug("EAS_TYPE_GET_EMAIL_ATTACHMENT_MSG ++");
+	EasGetEmailAttachmentMsg* self_eas_get_email_attachment_msg = g_object_new (EAS_TYPE_GET_EMAIL_ATTACHMENT_MSG, NULL);
+	g_object_ref(self_eas_get_email_attachment_msg);
+	g_object_unref(self_eas_get_email_attachment_msg);
+	g_object_unref(self_eas_get_email_attachment_msg);	
+	g_debug("EAS_TYPE_GET_EMAIL_ATTACHMENT_MSG --");
+
+	g_debug("EAS_TYPE_GET_EMAIL_ATTACHMENT_REQ ++");
+	EasGetEmailAttachmentReq* self_eas_get_email_attachment_req = g_object_new (EAS_TYPE_GET_EMAIL_ATTACHMENT_REQ, NULL);
+	g_object_ref(self_eas_get_email_attachment_req);
+	g_object_unref(self_eas_get_email_attachment_req);
+	g_object_unref(self_eas_get_email_attachment_req);	
+	g_debug("EAS_TYPE_GET_EMAIL_ATTACHMENT_REQ --");
+
+	g_debug("EAS_TYPE_GET_EMAIL_BODY_MSG ++");
+	EasGetEmailBodyMsg* self_eas_get_email_body_msg = g_object_new (EAS_TYPE_GET_EMAIL_BODY_MSG, NULL);
+	g_object_ref(self_eas_get_email_body_msg);
+	g_object_unref(self_eas_get_email_body_msg);
+	g_object_unref(self_eas_get_email_body_msg);	
+	g_debug("EAS_TYPE_GET_EMAIL_BODY_MSG --");
+
+	g_debug("EAS_TYPE_GET_EMAIL_BODY_REQ ++");
+	EasGetEmailBodyReq* self_eas_get_email_body_req = g_object_new (EAS_TYPE_GET_EMAIL_BODY_REQ, NULL);
+	g_object_ref(self_eas_get_email_body_req);
+	g_object_unref(self_eas_get_email_body_req);
+	g_object_unref(self_eas_get_email_body_req);	
+	g_debug("EAS_TYPE_GET_EMAIL_BODY_REQ --");
+
+	g_debug("EAS_TYPE_MOVE_EMAIL_MSG ++");
+	EasMoveEmailMsg* self_eas_move_email_msg = g_object_new (EAS_TYPE_MOVE_EMAIL_MSG, NULL);
+	g_object_ref(self_eas_move_email_msg);
+	g_object_unref(self_eas_move_email_msg);
+	g_object_unref(self_eas_move_email_msg);	
+	g_debug("EAS_TYPE_MOVE_EMAIL_MSG --");
+
+	g_debug("EAS_TYPE_MOVE_EMAIL_REQ ++");
+	EasMoveEmailReq* self_eas_move_email_req = g_object_new (EAS_TYPE_MOVE_EMAIL_REQ, NULL);
+	g_object_ref(self_eas_move_email_req);
+	g_object_unref(self_eas_move_email_req);
+	g_object_unref(self_eas_move_email_req);	
+	g_debug("EAS_TYPE_MOVE_EMAIL_REQ --");
+
+
+	g_debug("EAS_TYPE_MSG_BASE ++");
+	EasMsgBase* self_eas_msg_base = g_object_new (EAS_TYPE_MSG_BASE, NULL);
+	g_object_ref(self_eas_msg_base);
+	g_object_unref(self_eas_msg_base);
+	g_object_unref(self_eas_msg_base);	
+	g_debug("EAS_TYPE_MSG_BASE --");
+
+
+	g_debug("EAS_TYPE_PING_MSG ++");
+	EasPingMsg* self_eas_ping_msg = g_object_new (EAS_TYPE_PING_MSG, NULL);
+	g_object_ref(self_eas_ping_msg);
+	g_object_unref(self_eas_ping_msg);
+	g_object_unref(self_eas_ping_msg);	
+	g_debug("EAS_TYPE_PING_MSG --");
+
+	g_debug("EAS_TYPE_PING_REQ ++");
+	EasPingReq* self_eas_ping_req = g_object_new (EAS_TYPE_PING_REQ, NULL);
+	g_object_ref(self_eas_ping_req);
+	g_object_unref(self_eas_ping_req);
+	g_object_unref(self_eas_ping_req);	
+	g_debug("EAS_TYPE_PING_REQ --");
+
+	g_debug("EAS_TYPE_PROVISION_MSG ++");
+	EasProvisionMsg* self_eas_provision_msg = g_object_new (EAS_TYPE_PROVISION_MSG, NULL);
+	g_object_ref(self_eas_provision_msg);
+	g_object_unref(self_eas_provision_msg);
+	g_object_unref(self_eas_provision_msg);	
+	g_debug("EAS_TYPE_PROVISION_MSG --");
+
+	g_debug("EAS_TYPE_PROVISION_REQ ++");
+	EasProvisionReq* self_eas_provision_req = g_object_new (EAS_TYPE_PROVISION_REQ, NULL);
+	g_object_ref(self_eas_provision_req);
+	g_object_unref(self_eas_provision_req);
+	g_object_unref(self_eas_provision_req);	
+	g_debug("EAS_TYPE_PROVISION_REQ --");
+
+	g_debug("EAS_TYPE_REQUEST_BASE ++");
+	EasRequestBase* self_eas_request_base = g_object_new (EAS_TYPE_REQUEST_BASE, NULL);
+	g_object_ref(self_eas_request_base);
+	g_object_unref(self_eas_request_base);
+	g_object_unref(self_eas_request_base);	
+	g_debug("EAS_TYPE_REQUEST_BASE --");
+
+	g_debug("EAS_TYPE_SEND_EMAIL_MSG ++");
+	EasSendEmailMsg* self_eas_send_email_msg = g_object_new (EAS_TYPE_SEND_EMAIL_MSG, NULL);
+	g_object_ref(self_eas_send_email_msg);
+	g_object_unref(self_eas_send_email_msg);
+	g_object_unref(self_eas_send_email_msg);	
+	g_debug("EAS_TYPE_SEND_EMAIL_MSG --");
+
+	g_debug("EAS_TYPE_SEND_EMAIL_REQ ++");
+	EasSendEmailReq* self_eas_send_email_req = g_object_new (EAS_TYPE_SEND_EMAIL_REQ, NULL);
+	g_object_ref(self_eas_send_email_req);
+	g_object_unref(self_eas_send_email_req);
+	g_object_unref(self_eas_send_email_req);	
+	g_debug("EAS_TYPE_SEND_EMAIL_REQ --");
+
+	g_debug("EAS_TYPE_SYNC_FOLDER_HIERARCHY_REQ ++");
+	EasSyncFolderHierarchyReq* self_eas_sync_folder_hierarchy_req = g_object_new (EAS_TYPE_SYNC_FOLDER_HIERARCHY_REQ, NULL);
+	g_object_ref(self_eas_sync_folder_hierarchy_req);
+	g_object_unref(self_eas_sync_folder_hierarchy_req);
+	g_object_unref(self_eas_sync_folder_hierarchy_req);	
+	g_debug("EAS_TYPE_SYNC_FOLDER_HIERARCHY_REQ --");
+
+	g_debug("EAS_TYPE_SYNC_FOLDER_MSG ++");
+	EasSyncFolderMsg* self_eas_sync_folder_msg = g_object_new (EAS_TYPE_SYNC_FOLDER_MSG, NULL);
+	g_object_ref(self_eas_sync_folder_msg);
+	g_object_unref(self_eas_sync_folder_msg);
+	g_object_unref(self_eas_sync_folder_msg);	
+	g_debug("EAS_TYPE_SYNC_FOLDER_MSG --");
+
+	g_debug("EAS_TYPE_SYNC__MSG ++");
+	EasSyncMsg* self_eas_sync_msg = g_object_new (EAS_TYPE_SYNC_MSG, NULL);
+	g_object_ref(self_eas_sync_msg);
+	g_object_unref(self_eas_sync_msg);
+	g_object_unref(self_eas_sync_msg);	
+	g_debug("EAS_TYPE_SYNC_MSG --");
+
+	g_debug("EAS_TYPE_SYNC__REQ ++");
+	EasSyncReq* self_eas_sync_req = g_object_new (EAS_TYPE_SYNC_REQ, NULL);
+	g_object_ref(self_eas_sync_req);
+	g_object_unref(self_eas_sync_req);
+	g_object_unref(self_eas_sync_req);	
+	g_debug("EAS_TYPE_SYNC_REQ --");
+
+	g_debug("EAS_TYPE_UPDATE_EMAIL_REQ ++");
+	EasUpdateEmailReq* self_eas_update_email_req = g_object_new(EAS_TYPE_UPDATE_EMAIL_REQ, NULL);
+	g_object_ref(self_eas_update_email_req);
+	g_object_unref(self_eas_update_email_req);
+	g_object_unref(self_eas_update_email_req);	
+	g_debug("EAS_TYPE_UPDATE_EMAIL_REQ --");
+
+	g_debug("EAS_TYPE_UPDATE_ITEM_REQ ++");
+	EasUpdateItemReq* self_eas_update_item_req = g_object_new(EAS_TYPE_UPDATE_ITEM_REQ, NULL);
+	g_object_ref(self_eas_update_item_req);
+	g_object_unref(self_eas_update_item_req);
+	g_object_unref(self_eas_update_item_req);	
+	g_debug("EAS_TYPE_UPDATE_ITEM_REQ --");
+
+	g_debug("EAS_TYPE_MAIL ++");
+	EasMail* self_eas_mail = g_object_new(EAS_TYPE_MAIL, NULL);
+	g_object_ref(self_eas_mail);
+	g_object_unref(self_eas_mail);
+	g_object_unref(self_eas_mail);	
+	g_debug("EAS_TYPE_MAIL --");
+
+	g_debug("EAS_TYPE_SYNC ++");
+	EasSync* self_eas_sync = g_object_new(EAS_TYPE_SYNC, NULL);
+	g_object_ref(self_eas_sync);
+	g_object_unref(self_eas_sync);
+	g_object_unref(self_eas_sync);	
+	g_debug("EAS_TYPE_SYNC --");
+
+	g_debug("EAS_TYPE_TEST ++");
+	EasTest* self_eas_test = g_object_new(EAS_TYPE_TEST, NULL);
+	g_object_ref(self_eas_test);
+	g_object_unref(self_eas_test);
+	g_object_unref(self_eas_test);	
+	g_debug("EAS_TYPE_TEST --"); 
+
+	g_debug("EAS_TYPE_ACCOUNT_LIST ++");
+	EasAccountList* self_eas_account_list = g_object_new(EAS_TYPE_ACCOUNT_LIST, NULL);
+	g_object_ref(self_eas_account_list);
+	g_object_unref(self_eas_account_list);
+	g_object_unref(self_eas_account_list);	
+	g_debug("EAS_TYPE_ACCOUNT_LIST --"); 
+
+	g_debug("EAS_TYPE_ACCOUNT ++");
+	EasAccount* self_eas_account = g_object_new(EAS_TYPE_ACCOUNT, NULL);
+	g_object_ref(self_eas_account);
+	g_object_unref(self_eas_account);
+	g_object_unref(self_eas_account);	
+	g_debug("EAS_TYPE_ACCOUNT --"); 
+
+	g_debug("EAS_TYPE_ATTACHMENT ++");
+	EasAttachment* self_eas_attachment = g_object_new(EAS_TYPE_ATTACHMENT, NULL);
+	g_object_ref(self_eas_attachment);
+	g_object_unref(self_eas_attachment);
+	g_object_unref(self_eas_attachment);	
+	g_debug("EAS_TYPE_ATTACHMENT --");
+
+	g_debug("EAS_TYPE_EMAIL_INFO ++");
+	EasEmailInfo* self_eas_email_info = g_object_new(EAS_TYPE_EMAIL_INFO, NULL);
+	g_object_ref(self_eas_email_info);
+	g_object_unref(self_eas_email_info);
+	g_object_unref(self_eas_email_info);	
+	g_debug("EAS_TYPE_EMAIL_INFO --");
+
+	g_debug("EAS_TYPE_FOLDER ++");
+	EasFolder* self_eas_folder = g_object_new(EAS_TYPE_FOLDER, NULL);
+	g_object_ref(self_eas_folder);
+	g_object_unref(self_eas_folder);
+	g_object_unref(self_eas_folder);	
+	g_debug("EAS_TYPE_FOLDER --");
+
+	g_debug("EAS_TYPE_EMAIL_HANDLER ++");
+	EasEmailHandler* self_eas_email_handler = g_object_new(EAS_TYPE_EMAIL_HANDLER, NULL);
+	g_object_ref(self_eas_email_handler);
+	g_object_unref(self_eas_email_handler);
+	g_object_unref(self_eas_email_handler);	
+	g_debug("EAS_TYPE_EMAIL_HANDLER --");
+
+	g_debug("EAS_TYPE_ITEM_INFO ++");
+	EasSyncHandler* self_eas_item_info = g_object_new(EAS_TYPE_ITEM_INFO	, NULL);
+	g_object_ref(self_eas_item_info);
+	g_object_unref(self_eas_item_info);
+	g_object_unref(self_eas_item_info);	
+	g_debug("EAS_TYPE_ITEM_INFO --");
+
+	g_debug("EAS_TYPE_SYNC_HANDLER ++");
+	EasSyncHandler* self_eas_sync_handler = g_object_new(EAS_TYPE_SYNC_HANDLER	, NULL);
+	g_object_ref(self_eas_sync_handler);
+	g_object_unref(self_eas_sync_handler);
+	g_object_unref(self_eas_sync_handler);	
+	g_debug("EAS_TYPE_SYNC_HANDLER --");
+
+
+	g_debug("EAS_TYPE_TEST_HANDLER ++");
+	EasTestHandler* self_eas_test_handler = g_object_new(EAS_TYPE_TEST_HANDLER	, NULL);
+	g_object_ref(self_eas_test_handler);
+	g_object_unref(self_eas_test_handler);
+	g_object_unref(self_eas_test_handler);	
+	g_debug("EAS_TYPE_TEST_HANDLER --");
+
 }
 END_TEST
 
