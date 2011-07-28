af116ea36e59ecd8490a98402096e5436b1787c4
sohail.ahmad <sohail@mobica0109.mobicauk.local>
2011-07-28 13:46:34 +0100

    uncommented gobject test suite

--- check_tests/eas_main_tests.c
+++ check_tests/eas_main_tests.c
@@ -28,9 +28,9 @@ int main (void)
 //    srunner_add_suite (sr, eas_libeascal_suite());
 //    srunner_add_suite (sr, eas_libeassync_suite());
 //    srunner_add_suite (sr, eas_libeascon_suite()); 
-	//srunner_add_suite (sr, eas_gobjectunittest_suite()); 
-    srunner_add_suite (sr, eas_con_info_translator_suite()); 
-    srunner_add_suite (sr, eas_cal_info_translator_suite()); 
+	srunner_add_suite (sr, eas_gobjectunittest_suite()); 
+   	srunner_add_suite (sr, eas_con_info_translator_suite()); 
+    	srunner_add_suite (sr, eas_cal_info_translator_suite()); 
 
     srunner_set_xml (sr, "eas-daemon_test.xml");
     srunner_set_log (sr, "eas-daemon_test.log");
