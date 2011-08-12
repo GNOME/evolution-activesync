0c45f2e17d004571920f0bca0a21a08f2167f331
Sohail Ahmad <sohail.ahmad@mobica.com>
2011-08-12 13:23:03 +0100

    mock test for EmailBodyInvalidMimeDirectory error

--- check_tests/eas_libeasmail_tests.c
+++ check_tests/eas_libeasmail_tests.c
@@ -2119,6 +2119,32 @@ START_TEST (test_get_eas_mail_attachment_invalid_mime_directory)
 
 }
 END_TEST 
+START_TEST (test_get_eas_mail_body_invalid_mime_directory)
+{
+	const gchar* accountuid = g_account_id;
+	EasEmailHandler *email_handler = NULL;
+	GError *error = NULL;
+
+
+	// get a handle to the DBus interface and associate the account ID with this object
+	testGetMailHandler (&email_handler, accountuid);
+
+
+	// set mock
+	setMockNegTestGoodHttp("EmailBodyInvalidMimeDirectory.xml");
+
+	// mock Test
+	eas_mail_handler_fetch_email_attachment (email_handler,"mockTest","mockTest","mockTest","mockTest",&error);
+	
+	g_debug("error is %s",dbus_g_error_get_name(error));
+	fail_if(g_strcmp0 (dbus_g_error_get_name(error),         
+	                   "org.meego.activesyncd.Error.FILEERROR"),  
+	        "The Error returned by the server is not correct.");
+	
+	g_object_unref (email_handler);
+
+}
+END_TEST 
 Suite* eas_libeasmail_suite (void)
 {
     Suite* s = suite_create ("libeasmail");
@@ -2143,6 +2169,7 @@ Suite* eas_libeasmail_suite (void)
 		tcase_add_test (tc_libeasmail, test_get_eas_mail_delete_invalid_server_id);
 		tcase_add_test (tc_libeasmail, test_get_eas_mail_body_invalid_server_id);
 		tcase_add_test (tc_libeasmail, test_get_eas_mail_attachment_invalid_mime_directory);
+		tcase_add_test (tc_libeasmail, test_get_eas_mail_body_invalid_mime_directory);
 	}
  //   tcase_add_test (tc_libeasmail, test_get_eas_mail_info_in_inbox);
 //    tcase_add_test (tc_libeasmail, test_eas_mail_handler_fetch_email_body);
