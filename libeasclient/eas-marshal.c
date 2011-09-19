2dcb901afeb8d44543d1a6b46d95d90fdca31732
David Woodhouse <David.Woodhouse@intel.com>
2011-09-19 16:44:12 +0100

    Remove generated eas-marshal.[ch] files

--- libeasclient/eas-marshal.c
+++ libeasclient/eas-marshal.c
@@ -53,34 +53,37 @@
 /* VOID:UINT,UINT (./eas-marshal.list:1) */
 void
 eas_marshal_VOID__UINT_UINT (GClosure     *closure,
-			     GValue       *return_value G_GNUC_UNUSED,
-			     guint         n_param_values,
-			     const GValue *param_values,
-			     gpointer      invocation_hint G_GNUC_UNUSED,
-			     gpointer      marshal_data)
+                             GValue       *return_value G_GNUC_UNUSED,
+                             guint         n_param_values,
+                             const GValue *param_values,
+                             gpointer      invocation_hint G_GNUC_UNUSED,
+                             gpointer      marshal_data)
 {
-	typedef void (*GMarshalFunc_VOID__UINT_UINT) (gpointer     data1,
-						      guint        arg_1,
-						      guint        arg_2,
-						      gpointer     data2);
-	register GMarshalFunc_VOID__UINT_UINT callback;
-	register GCClosure *cc = (GCClosure*) closure;
-	register gpointer data1, data2;
+  typedef void (*GMarshalFunc_VOID__UINT_UINT) (gpointer     data1,
+                                                guint        arg_1,
+                                                guint        arg_2,
+                                                gpointer     data2);
+  register GMarshalFunc_VOID__UINT_UINT callback;
+  register GCClosure *cc = (GCClosure*) closure;
+  register gpointer data1, data2;
 
-	g_return_if_fail (n_param_values == 3);
+  g_return_if_fail (n_param_values == 3);
 
-	if (G_CCLOSURE_SWAP_DATA (closure)) {
-		data1 = closure->data;
-		data2 = g_value_peek_pointer (param_values + 0);
-	} else {
-		data1 = g_value_peek_pointer (param_values + 0);
-		data2 = closure->data;
-	}
-	callback = (GMarshalFunc_VOID__UINT_UINT) (marshal_data ? marshal_data : cc->callback);
+  if (G_CCLOSURE_SWAP_DATA (closure))
+    {
+      data1 = closure->data;
+      data2 = g_value_peek_pointer (param_values + 0);
+    }
+  else
+    {
+      data1 = g_value_peek_pointer (param_values + 0);
+      data2 = closure->data;
+    }
+  callback = (GMarshalFunc_VOID__UINT_UINT) (marshal_data ? marshal_data : cc->callback);
 
-	callback (data1,
-		  g_marshal_value_peek_uint (param_values + 1),
-		  g_marshal_value_peek_uint (param_values + 2),
-		  data2);
+  callback (data1,
+            g_marshal_value_peek_uint (param_values + 1),
+            g_marshal_value_peek_uint (param_values + 2),
+            data2);
 }
 
